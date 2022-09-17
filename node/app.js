0595584cce77851080752d515012ee843c5e29b2
AnhNg <s3877798@rmit.edu.vn>
2022-09-17 10:18:10 +0700

    nide

--- node/app.js
+++ node/app.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("/add", function(req, res){
-    let a = parseInt(req.query.a);
-    let b = parseInt(req.query.b);
-    let c = (a + b).toString();
-    res.send(c);
-})
+app.get('/', function(request, response){
+    response.sendFile(__dirname + '/index.html');
+});
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("port 3000");
